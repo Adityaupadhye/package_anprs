57fe8c82e19db4c91738988576a66808abaecf75
Aditya Upadhye <adityasu12@gmail.com>
2024-03-05 10:23:58 +0530

    version bump and modify event trigger to push main

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anprs',
-    version='0.0.1',
+    version='0.0.2',
     author='Aditya Upadhye',
     author_email='adityasu12@gmail.com',
     description='Automatic Number Plate Recognition System',
